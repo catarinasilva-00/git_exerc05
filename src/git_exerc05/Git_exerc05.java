c400ab9bd88c4c1dd48beaf8472c44199ed9942d
catarinasilva-00 <61985612+catarinasilva-00@users.noreply.github.com>
2020-03-09 21:06:06 +0000

    6

--- src/git_exerc05/Git_exerc05.java
+++ src/git_exerc05/Git_exerc05.java
@@ -15,7 +15,11 @@ public class Git_exerc05 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Porto p = new Porto(3);
+        Petroleiro pe= new Petroleiro("MA");
+        PortaContentores pc = new PortaContentores("MB");
+        
+        
     }
     
 }

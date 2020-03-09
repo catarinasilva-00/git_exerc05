5b307565fb1e46770bf0274d5d62ae0bc940ee1e
catarinasilva-00 <61985612+catarinasilva-00@users.noreply.github.com>
2020-03-09 21:05:55 +0000

    5

---
+++ src/git_exerc05/Porto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_exerc05;
+
+/**
+ *
+ * @author catar
+ */
+public class Porto {
+    int capPorto;
+    
+    public Porto(int capPorto){
+        this.capPorto=capPorto;
+    }
+}
